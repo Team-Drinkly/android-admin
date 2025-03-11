7fff74399bad38816a70c7f28f1ed2bf8db780e1
PIYUJIN <yjyj020201@gmail.com>
2025-03-11 21:58:00 +0900

    [Setting] #1  API 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,15 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 }
+
+val properties = Properties().apply {
+    load(FileInputStream(rootProject.file("local.properties")))
+}
+val kakaoNativeKey = properties.getProperty("kakao_app_key")
 
 android {
     namespace = "com.project.drinkly_admin"
@@ -15,6 +23,18 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "KAKAO_APP_KEY", "\"${properties["kakao_key"]}\"")
+
+        manifestPlaceholders["kakao_native_key"] = kakaoNativeKey
+    }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    dataBinding {
+        enable = true
     }
 
     buildTypes {
@@ -45,4 +65,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // api
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // JSON 변환
+    implementation("com.squareup.okhttp3:okhttp:4.10.0") // OkHttp 라이브러리
+    implementation("com.squareup.okhttp3:logging-interceptor:3.11.0")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.9.0")
+
+    // glide
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
 }
--- app/src/main/java/com/project/drinkly_admin/ui/MainActivity.kt
+++ app/src/main/java/com/project/drinkly_admin/ui/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.project.drinkly_admin.ui
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.project.drinkly_admin.R
+import com.project.drinkly_admin.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun getKeyHash() {
+        try {
+            val info = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            for (signature in info.signingInfo?.apkContentsSigners!!) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                val keyHash = Base64.encodeToString(md.digest(), Base64.NO_WRAP)
+                Log.d("KeyHash", keyHash)  // 키 해시를 로그로 출력
+            }
+        } catch (e: NoSuchAlgorithmException) {
+            Log.e("KeyHash", "Unable to get MessageDigest. signature= $e")
+        } catch (e: Exception) {
+            Log.e("KeyHash", "Exception: $e")
         }
     }
 }
