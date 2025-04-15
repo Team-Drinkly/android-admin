ef0d7597ed7872a258b3477aa3bc0e15083ecc49
PIYUJIN <yjyj020201@gmail.com>
2025-04-16 01:30:13 +0900

    [Feat] #2  회원가입 - 매장 정보 입력 화면 구성

---
+++ app/src/main/java/com/project/drinkly_admin/ui/signUp/SignUpStoreInfoFragment.kt
@@ -0,0 +1,95 @@
+package com.project.drinkly_admin.ui.signUp
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.drinkly_admin.R
+import com.project.drinkly_admin.databinding.FragmentSignUpStoreInfoBinding
+import com.project.drinkly_admin.ui.MainActivity
+
+class SignUpStoreInfoFragment : Fragment() {
+
+    lateinit var binding: FragmentSignUpStoreInfoBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSignUpStoreInfoBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        binding.run {
+            editTextStoreName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                    if(editTextStoreName.text.isNotEmpty()) {
+                        editTextStoreName.setBackgroundResource(R.drawable.background_edittext_success)
+                    } else {
+                        editTextStoreName.setBackgroundResource(R.drawable.background_edittext_default)
+                    }
+
+                    checkEnabled()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+
+            editTextStoreAddressMain.setOnClickListener {
+                // 도로명 주소 검색 기능
+            }
+
+            editTextStoreAddressDetail.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                    if(editTextStoreAddressDetail.text.isNotEmpty()) {
+                        editTextStoreAddressDetail.setBackgroundResource(R.drawable.background_edittext_success)
+                    } else {
+                        editTextStoreAddressDetail.setBackgroundResource(R.drawable.background_edittext_default)
+                    }
+
+                    checkEnabled()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+        }
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initView()
+    }
+
+    fun checkEnabled() {
+        binding.run {
+            if(editTextStoreName.text.isNotEmpty() && editTextStoreAddressMain.text.isNotEmpty()) {
+                buttonNext.isEnabled = true
+            } else {
+                buttonNext.isEnabled = false
+            }
+        }
+    }
+
+    fun initView() {
+        binding.run {
+            toolbar.run {
+                textViewTitle.text = "매장 정보 입력"
+                buttonBack.setOnClickListener {
+                    fragmentManager?.popBackStack()
+                }
+            }
+        }
+    }
+}
