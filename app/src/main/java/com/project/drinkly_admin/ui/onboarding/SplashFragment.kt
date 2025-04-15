98cd161bdf126edffd859907758416a42270b44e
PIYUJIN <yjyj020201@gmail.com>
2025-04-16 00:59:54 +0900

    [Feat] #2  splash 화면 기능 구현

--- app/src/main/java/com/project/drinkly_admin/ui/onboarding/SplashFragment.kt
+++ app/src/main/java/com/project/drinkly_admin/ui/onboarding/SplashFragment.kt
@@ -1,20 +1,35 @@
 package com.project.drinkly_admin.ui.onboarding
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.drinkly_admin.R
+import com.project.drinkly_admin.databinding.FragmentSplashBinding
+import com.project.drinkly_admin.ui.MainActivity
 
 class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        Handler().postDelayed({
+            mainActivity.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView_main, LoginFragment())
+                .commit()
+        }, 3000)
+
+        return binding.root
     }
 
 }
