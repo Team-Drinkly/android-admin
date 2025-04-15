c8392fe42c65603dd556e13acfd915787a749eb6
PIYUJIN <yjyj020201@gmail.com>
2025-04-16 01:30:34 +0900

    [Feat] #2  회원가입 - 매장 번호 입력 화면 구성

---
+++ app/src/main/java/com/project/drinkly_admin/ui/signUp/SignUpStoreNumberFragment.kt
@@ -0,0 +1,65 @@
+package com.project.drinkly_admin.ui.signUp
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.drinkly_admin.R
+import com.project.drinkly_admin.databinding.FragmentSignUpStoreNumberBinding
+import com.project.drinkly_admin.ui.MainActivity
+
+class SignUpStoreNumberFragment : Fragment() {
+
+    lateinit var binding: FragmentSignUpStoreNumberBinding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSignUpStoreNumberBinding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        binding.run {
+            editTextStoreNumber.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                    if(editTextStoreNumber.text.isNotEmpty()) {
+                        editTextStoreNumber.setBackgroundResource(R.drawable.background_edittext_success)
+                        buttonNext.isEnabled = true
+                    } else {
+                        editTextStoreNumber.setBackgroundResource(R.drawable.background_edittext_default)
+                        buttonNext.isEnabled = false
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+
+        }
+
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initView()
+    }
+
+    fun initView() {
+        binding.run {
+            toolbar.run {
+                textViewTitle.text = "매장 정보 입력"
+                buttonBack.setOnClickListener {
+                    fragmentManager?.popBackStack()
+                }
+            }
+        }
+    }
+}
