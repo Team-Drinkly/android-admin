4fe180afe683cc0100f9f3258f9b824cde8f1c45
PIYUJIN <yjyj020201@gmail.com>
2025-04-21 03:17:38 +0900

    [Fix] #2  카카오 로그인 기능 오류 수정

--- app/src/main/java/com/project/drinkly_admin/util/MyApplication.kt
+++ app/src/main/java/com/project/drinkly_admin/util/MyApplication.kt
@@ -1,6 +1,7 @@
-package com.project.drinkly_admin
+package com.project.drinkly_admin.util
 
 import android.app.Application
+import com.project.drinkly_admin.api.request.login.BasicStoreInfoRequest
 
 class MyApplication : Application() {
     companion object {
